7099ab04d12e1dc5077c624cc3cf1a1a9827dc66
Roma Bulani <romabulani@gmail.com>
2022-03-16 00:23:10 +0530

    docs : improvised folder structure

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { CartPage } from "./CartPage";
+export { LandingPage } from "./LandingPage";
+export { LoginPage } from "./LoginPage";
+export { PasswordResetPage } from "./PasswordResetPage";
+export { ProductListingPage } from "./ProductListingPage";
+export { SignupPage } from "./SignupPage";
+export { WishlistPage } from "./WishlistPage";
