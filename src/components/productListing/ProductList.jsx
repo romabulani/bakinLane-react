b11721b840bc7a633b6859c2dbd6bb328f7953b1
Roma Bulani <romabulani@gmail.com>
2022-03-16 00:22:08 +0530

    fix : removed duplicate component

--- src/components/productListing/ProductList.jsx
+++ src/components/productListing/ProductList.jsx
@@ -2,19 +2,13 @@ import { Filters } from "./Filters";
 import "./productlist.css";
 import { Products } from "./Products";
 import "../../styles/globalbakin.css";
-import { Navigation } from "../shared/Navigation";
-import { Footer } from "../shared/Footer";
 
 function ProductList() {
   return (
-    <div className="pagewrapper">
-      <Navigation />
-      <main className="filters-and-products middle-content">
-        <Filters className="filters" />
-        <Products />
-      </main>
-      <Footer />
-    </div>
+    <main className="filters-and-products middle-content">
+      <Filters className="filters" />
+      <Products />
+    </main>
   );
 }
 
