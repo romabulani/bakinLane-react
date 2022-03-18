68cf9c76bc9131eb19acf5c66bf000f1fbc53dcf
Roma Bulani <romabulani@gmail.com>
2022-03-18 20:25:21 +0530

    feat: Profile Page

---
+++ src/components/profile/ProfileDetails.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { useAuth } from "../../contexts";
+import { useNavigate } from "react-router-dom";
+import "./profile.css";
+
+function ProfileDetails() {
+  const { setAuthToken, authUser, setAuthUser } = useAuth();
+  const navigate = useNavigate();
+  function logoutHandler() {
+    localStorage.removeItem("authToken");
+    localStorage.removeItem("authUser");
+    setAuthToken("");
+    setAuthUser(null);
+    navigate("/products");
+  }
+
+  return (
+    <div className="flex-row-center">
+      <div className="logout-container flex-column-center">
+        <h4 className="heading4">ACCOUNT DETAILS</h4>
+        <div className="row-format">
+          Name :{` ${authUser.firstName} ${authUser.lastName}`}
+        </div>
+        <div className="row-format">Email :{` ${authUser.email}`}</div>
+        <button className="btn btn-outline-default" onClick={logoutHandler}>
+          Log Out
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export { ProfileDetails };
