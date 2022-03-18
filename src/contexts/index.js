8cffe87cee2bc21ebbf58b25590647a9cd9fa975
Roma Bulani <romabulani@gmail.com>
2022-03-18 20:21:24 +0530

    feat: authContext

--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,2 +1,3 @@
 export { ProductsDataProvider, useProductsData } from "./dataContext";
 export { FilterProvider, useFilters } from "./filterContext";
+export { AuthProvider, useAuth } from "./authContext";
