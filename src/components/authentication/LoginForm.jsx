9e83552f57f2520c7a0156365a36dbcadb422691
Roma Bulani <romabulani@gmail.com>
2022-03-21 18:12:53 +0530

    feat : jsx files for auth

--- src/components/authentication/LoginForm.jsx
+++ src/components/authentication/LoginForm.jsx
@@ -1,92 +1,96 @@
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useLoginHandler } from "hooks";
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
-import { useAuth } from "../../contexts";
-import { loginService } from "../../services";
+import { Link } from "react-router-dom";
 import "./auth.css";
 
 function LoginForm() {
-  const { setAuthToken, setAuthUser } = useAuth();
-  const navigate = useNavigate();
   const [loginData, setLoginData] = useState({ email: "", password: "" });
-
-  const loginHandler = async (e) => {
-    try {
-      let response;
-      if (e.target.innerText === "Login with Test Credentials") {
-        setLoginData({
-          email: "adarshbalika@gmail.com",
-          password: "adarshBalika123",
-        });
-        response = await loginService(
-          "adarshbalika@gmail.com",
-          "adarshBalika123"
-        );
-      } else response = await loginService(loginData.email, loginData.password);
-      const user = JSON.stringify(response.foundUser);
-      const tokenResponse = response.encodedToken;
-      setAuthToken(tokenResponse);
-      setAuthUser(response.foundUser);
-      localStorage.setItem("authToken", tokenResponse);
-      localStorage.setItem("authUser", user);
-      navigate("/products");
-    } catch (e) {
-      console.log("loginHandler: Error in Login");
-    }
-  };
-
+  const [errorData, setErrorData] = useState(false);
+  const { loginHandler } = useLoginHandler();
   return (
-    <main className="content-container">
-      <div className="flex-row-center">
-        <div className="auth-container flex-column-center">
-          <h4 className="heading4">LOGIN</h4>
-          <div className="form-auth">
-            <input
-              type="email"
-              placeholder="Enter email"
-              className="input-primary border-box"
-              value={loginData.email}
-              onChange={(e) =>
-                setLoginData((loginData) => ({
-                  ...loginData,
-                  email: e.target.value,
-                }))
-              }
-            />
-            <input
-              type="password"
-              placeholder="Enter password"
-              className="input-primary border-box"
-              value={loginData.password}
-              onChange={(e) =>
-                setLoginData((loginData) => ({
-                  ...loginData,
-                  password: e.target.value,
-                }))
-              }
-            />
-            <button className="btn btn-primary btn-auth">Login</button>
-            <button
-              className="btn btn-outline-primary btn-auth"
-              onClick={loginHandler}
-            >
-              Login with Test Credentials
-            </button>
-            <div>
-              <span>Don't have an account?</span>
-              <Link to="/signup" className="btn-link btn-link-primary">
-                Create One
-              </Link>
-            </div>
-            <div>
-              <span>Forgot Password?</span>
-              <Link to="/passwordReset" className="btn-link btn-link-primary">
-                Reset here
-              </Link>
-            </div>
-          </div>
+    <div className="auth-container flex-column-center">
+      <h4 className="heading4">LOGIN</h4>
+      <form className="form-auth">
+        <div className="form-input">
+          <label htmlFor="email" className="input-label">
+            Email *
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Enter email"
+            className="input-primary border-box"
+            value={loginData.email}
+            onChange={(e) =>
+              setLoginData((loginData) => ({
+                ...loginData,
+                email: e.target.value,
+              }))
+            }
+            onFocus={() => setErrorData(false)}
+          />
         </div>
-      </div>
-    </main>
+        <div className="form-input">
+          {" "}
+          <label htmlFor="password" className="input-label">
+            Password *
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            className="input-primary border-box"
+            value={loginData.password}
+            onChange={(e) =>
+              setLoginData((loginData) => ({
+                ...loginData,
+                password: e.target.value,
+              }))
+            }
+            onFocus={() => setErrorData(false)}
+          />
+        </div>
+
+        <button
+          className="btn btn-primary btn-auth"
+          onClick={(e) =>
+            loginHandler(e, setLoginData, setErrorData, loginData)
+          }
+        >
+          Login
+        </button>
+        <button
+          className="btn btn-outline-primary btn-auth guest-button"
+          onClick={(e) => loginHandler(e, setLoginData, setErrorData)}
+        >
+          Login as Guest
+        </button>
+        {errorData && (
+          <div className="error">
+            <FontAwesomeIcon
+              icon={faCircleExclamation}
+              className="error-icon"
+            ></FontAwesomeIcon>
+            {"    "}
+            <div> Email or Password is incorrect</div>
+          </div>
+        )}
+        <div>
+          <span>Don't have an account?</span>
+          <Link to="/signup" className="btn-link btn-link-primary">
+            Create One
+          </Link>
+        </div>
+        <div>
+          <span>Forgot Password?</span>
+          <Link to="/passwordReset" className="btn-link btn-link-primary">
+            Reset here
+          </Link>
+        </div>
+      </form>
+    </div>
   );
 }
 
--- src/components/authentication/SignupForm.jsx
+++ src/components/authentication/SignupForm.jsx
@@ -1,58 +1,213 @@
 import "./auth.css";
 import { Link } from "react-router-dom";
+import { useSignupHandler } from "hooks";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleExclamation,
+  faExclamationCircle,
+} from "@fortawesome/free-solid-svg-icons";
 
 function SignupForm() {
+  const { formData, formDispatch, errorData, errorDispatch, signUpHandler } =
+    useSignupHandler();
+
   return (
-    <main className="content-container">
-      <div className="flex-row-center">
-        <div className="auth-container flex-column-center signup-container">
-          <h4 className="heading4">SIGN UP</h4>
-          <div action="" className="form-auth">
-            <input
-              type="text"
-              placeholder="Enter First Name"
-              className="input-primary border-box"
-              required
-            />
-            <input
-              type="text"
-              placeholder="Enter Last Name"
-              className="input-primary border-box"
-              required
-            />
-            <input
-              type="email"
-              placeholder="Enter email"
-              className="input-primary border-box"
-              required
-            />
-            <input
-              type="password"
-              placeholder="Enter password"
-              className="input-primary border-box"
-              required
-            />
-            <input
-              type="password"
-              placeholder="Confirm password"
-              className="input-primary border-box"
-              required
-            />
-            <input type="checkbox" id="terms-conditions" />
-            <label htmlFor="terms-conditions">
-              I accept all terms and conditions
-            </label>
-            <button className="btn btn-primary btn-auth">SIGN UP</button>
-            <div>
-              <span>Already have an account?</span>
-              <Link to="/login" className="btn-link btn-link-primary">
-                Login here
-              </Link>
-            </div>
-          </div>
+    <div className="auth-container flex-column-center signup-container">
+      <h4 className="heading4">SIGN UP</h4>
+      <form className="form-auth">
+        <div className="form-input ">
+          {" "}
+          <label htmlFor="firstName" className="input-label">
+            First Name *
+          </label>
+          <input
+            type="text"
+            placeholder="Enter First Name"
+            className="input-primary border-box"
+            id="firstName"
+            value={formData.firstName}
+            onChange={(e) =>
+              formDispatch({
+                type: "INPUT_FIRST_NAME",
+                payload: e.target.value,
+              })
+            }
+            onFocus={() =>
+              errorDispatch({
+                type: "ERROR_FIRST_NAME",
+                payload: "",
+              })
+            }
+            required
+          />
         </div>
-      </div>
-    </main>
+        {errorData.firstName.length > 0 && (
+          <div className="error">
+            <FontAwesomeIcon
+              icon={faCircleExclamation}
+              className="error-icon"
+            ></FontAwesomeIcon>
+            {"    "}
+            <div> {errorData.firstName}</div>
+          </div>
+        )}
+        <div className="form-input">
+          {" "}
+          <label htmlFor="firstName" className="input-label">
+            Last Name *
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Last Name"
+            id="lastName"
+            className="input-primary border-box"
+            value={formData.lastName}
+            onChange={(e) =>
+              formDispatch({
+                type: "INPUT_LAST_NAME",
+                payload: e.target.value,
+              })
+            }
+            onFocus={() =>
+              errorDispatch({
+                type: "ERROR_LAST_NAME",
+                payload: "",
+              })
+            }
+            required
+          />
+        </div>
+        {errorData.lastName.length > 0 && (
+          <div className="error">
+            <FontAwesomeIcon
+              icon={faCircleExclamation}
+              className="error-icon"
+            ></FontAwesomeIcon>
+            {"    "}
+            <div> {errorData.lastName}</div>
+          </div>
+        )}
+
+        <div className="form-input">
+          {" "}
+          <label htmlFor="email" className="input-label">
+            Email *
+          </label>
+          <input
+            placeholder="Enter email "
+            id="email"
+            className="input-primary border-box"
+            value={formData.email}
+            onChange={(e) =>
+              formDispatch({ type: "INPUT_EMAIL", payload: e.target.value })
+            }
+            onFocus={() =>
+              errorDispatch({
+                type: "ERROR_EMAIL",
+                payload: "",
+              })
+            }
+            required
+          />
+        </div>
+        {errorData.email.length > 0 && (
+          <div className="error">
+            <FontAwesomeIcon
+              icon={faCircleExclamation}
+              className="error-icon"
+            ></FontAwesomeIcon>
+            {"    "}
+            <div> {errorData.email}</div>
+          </div>
+        )}
+        <div className="form-input">
+          <label htmlFor="password" className="input-label">
+            Password *
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Enter password"
+            className="input-primary border-box"
+            value={formData.password}
+            onChange={(e) =>
+              formDispatch({
+                type: "INPUT_PASSWORD",
+                payload: e.target.value,
+              })
+            }
+            onFocus={() =>
+              errorDispatch({
+                type: "ERROR_PASSWORD",
+                payload: "",
+              })
+            }
+            required
+          />
+        </div>
+        {errorData.password.length > 0 && (
+          <div className="error">
+            <FontAwesomeIcon
+              icon={faCircleExclamation}
+              className="error-icon"
+            ></FontAwesomeIcon>
+            {"    "}
+            <div> {errorData.password}</div>
+          </div>
+        )}
+
+        <div className="form-input">
+          <label htmlFor="confirmpassword" className="input-label">
+            Confirm Password *
+          </label>
+          <input
+            type="password"
+            id="confirmpassword"
+            placeholder="Confirm password"
+            className="input-primary border-box"
+            value={formData.confirmPassword}
+            onChange={(e) =>
+              formDispatch({
+                type: "INPUT_CONFIRM_PASSWORD",
+                payload: e.target.value,
+              })
+            }
+            onFocus={() =>
+              errorDispatch({
+                type: "ERROR_CONFIRM_PASSWORD",
+                payload: "",
+              })
+            }
+            required
+          />
+        </div>
+        {errorData.confirmPassword.length > 0 && (
+          <div className="error">
+            <FontAwesomeIcon
+              icon={faCircleExclamation}
+              className="error-icon"
+            ></FontAwesomeIcon>
+            {"    "}
+            <div> {errorData.confirmPassword}</div>
+          </div>
+        )}
+
+        <button
+          type="submit"
+          className="btn btn-primary btn-auth"
+          onClick={(e) => signUpHandler(e)}
+        >
+          SIGN UP
+        </button>
+
+        <div className="flex-row-center">
+          <span>Already have an account?</span>
+          <Link to="/login" className="btn-link btn-link-primary">
+            Login here
+          </Link>
+        </div>
+      </form>
+    </div>
   );
 }
 
