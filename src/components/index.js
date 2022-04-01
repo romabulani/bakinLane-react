2e266416adfe0bf6af41a36fc82783530b1393e8
Roma Bulani <romabulani@gmail.com>
2022-04-02 01:08:47 +0530

    config: folder rename

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export { LoginForm } from "./authentication/LoginForm";
 export { SignupForm } from "./authentication/SignupForm";
 export { PasswordResetForm } from "./authentication/PasswordResetForm";
 export { HeroSection } from "./landingpage/HeroSection";
-export { ProductList } from "./productlisting/ProductList";
+export { ProductList } from "./productListing/ProductList";
 export { Navigation } from "./shared/Navigation";
 export { Footer } from "./shared/Footer";
 export { MockAPI } from "./shared/MockAPI";
